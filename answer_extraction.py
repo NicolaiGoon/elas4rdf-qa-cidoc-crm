1a69a995823f039d874cb094e0b9a58dba816037
nicolaigoon <xgounakis5@gmail.com>
2022-11-21 11:32:44 +0200

    change ATP pipeline

--- answer_extraction.py
+++ answer_extraction.py
@@ -16,7 +16,7 @@ class AnswerExtraction:
         # Obtain a question from each given entity
         answers = []
         for e in entities:
-            print('ans '+e['uri'])
+            # print('ans '+e['uri'])
             # print(e)
             # emtpy text
             if e['text'] == '' or e['text'] == ' ':  
@@ -37,35 +37,29 @@ class AnswerExtraction:
                     'text':''
                     })
                 else:
-                    highlighted_text = e['text'][0:output['start']]+'<b>'+e['text'][output['start']:output['end']]+'</b>'+e['text'][output['end']:] 
+                    # highlighted_text = e['text'][0:output['start']]+'<b>'+e['text'][output['start']:output['end']]+'</b>'+e['text'][output['end']:] 
                     answers.append({
                         'entity':e['uri'],
                         'answer':output['answer'],
                         'score':round(output['score'],3),
-                        'text':highlighted_text
+                        'text':e['text']
                         })
         return sorted(answers, key=lambda k: k['score'],reverse=True) 
 
     def extend_entities(self,entities,category,atype,depth=2):
-        # Extend entity descriptions with RDF nodes matching the answer type
+        # Extend entity descriptions with RDF nodes matching the answer type if it is not resource,
+        # and if it is resource create text from a subgraph starting from the resource
         extended = []
 
-        if(category=='resource'):
+        if(category=='resource' or (category == None and atype == None)):
             # type_uri = 'http://dbpedia.org/ontology/'+atype
             # sentences = expansion.resource_sentences(e['uri'],type_uri)
             return expansion.expandEntitiesPath(entities,depth)
         else:
             for e in entities:
-                print('ext '+e['uri'])
-                if(category=='literal'):
-                    sentences = expansion.literal_sentences(e['uri'],atype)
-                else:
-                    sentences = []
-                if(e['text'] == "[]"):
-                    clean_rdfs_comment = ""
-                else:
-                    clean_rdfs_comment = e['text'][3:-4]
-                new_text = clean_rdfs_comment + ". ".join(sentences)
+                # print('ext '+e['uri'])
+                sentences = set(expansion.literal_sentences(e['uri'],atype))
+                new_text = (e['text'] + " " + ". ".join(sentences)).strip()
                 # print(new_text)
                 extended.append({
                     'uri':e['uri'],
