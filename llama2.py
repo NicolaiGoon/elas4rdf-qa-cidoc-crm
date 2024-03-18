220bb0ae168571692f43a6b29fd256b57ceb269b
Nicolai Goon <xgounakis5@gmail.com>
2024-03-18 23:18:17 +0200

    remove unwanted stuff

--- llama2.py
+++ llama2.py
@@ -10,21 +10,32 @@ class Llama2:
         # self.client = Client("https://ysharma-explore-llamav2-with-tgi.hf.space/")
 
     def predict(self,question,passage):
-        self.client = Client("https://ysharma-explore-llamav2-with-tgi.hf.space/")
+        # self.client = Client("https://ysharma-explore-llamav2-with-tgi.hf.space/--replicas/9zqfs/")
+        self.client = Client("https://huggingface-projects-llama-2-13b-chat.hf.space/--replicas/5c42d8wx6/")
+
         self.get_response(f"Context: {passage}")
         return self.get_response(f"Question: {question}")
 
     def get_response(self,message):
+        # return self.client.predict(
+        #             message,	# str in 'Message' Textbox component
+        #             self.prompt,	# str in 'Optional system prompt' Textbox component
+        #             0.9,	# int | float (numeric value between 0.0 and 1.0)
+        #                             # in 'Temperature' Slider component
+        #             128,	# int | float (numeric value between 0 and 4096)
+        #                             # in 'Max new tokens' Slider component
+        #             0.6,	# int | float (numeric value between 0.0 and 1)
+        #                             # in 'Top-p (nucleus sampling)' Slider component
+        #             1.2,	# int | float (numeric value between 1.0 and 2.0)
+        #                             # in 'Repetition penalty' Slider component
+        #             api_name="/chat"
         return self.client.predict(
                     message,	# str in 'Message' Textbox component
                     self.prompt,	# str in 'Optional system prompt' Textbox component
-                    0.9,	# int | float (numeric value between 0.0 and 1.0)
-                                    # in 'Temperature' Slider component
-                    128,	# int | float (numeric value between 0 and 4096)
-                                    # in 'Max new tokens' Slider component
-                    0.6,	# int | float (numeric value between 0.0 and 1)
-                                    # in 'Top-p (nucleus sampling)' Slider component
-                    1.2,	# int | float (numeric value between 1.0 and 2.0)
-                                    # in 'Repetition penalty' Slider component
+                    128,	# int | float (numeric value between 1 and 2048) in 'Max new tokens' Slider component
+                    0.6,	# int | float (numeric value between 0.1 and 4.0) in 'Temperature' Slider component
+                    0.9,	# int | float (numeric value between 0.05 and 1.0) in 'Top-p (nucleus sampling)' Slider component
+                    50,	# int | float (numeric value between 1 and 1000) in 'Top-k' Slider component
+                    1.2,	# int | float (numeric value between 1.0 and 2.0) in 'Repetition penalty' Slider component
                     api_name="/chat"
     )
